299381bf746d7dad3c0a6286098961d31722357c
GreenTer <thenewlife@tut.by>
2017-08-27 22:25:53 +0300

    Change app2

--- app2.rb
+++ app2.rb
@@ -2,6 +2,9 @@ require 'sqlite3'
 
 db = SQLite3::Database.new 'test.sqlite'
 
-db.execute "INSERT INTO Cars (Nmae,Price) VALUES ('Jaguar', 18000)"
+db.execute "SELECT * FROM Cars" do |car|
+	puts car
+	puts "===="
+end
 
 db.close
